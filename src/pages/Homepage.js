425ef23af38861927d01fc04a18c6ff9f11b414a
Amy Shen <amyshen@seas.upenn.edu>
2022-02-28 23:51:06 -0500

    added common classes in index.css

---
+++ src/pages/Homepage.js
@@ -0,0 +1,24 @@
+import { React } from 'react';
+// import '../styles/Homepage.css';
+
+function Homepage() {
+
+	return (
+		<div className="Homepage">
+			<h1>TEST H1</h1>
+            erganglna;dn
+            <button className="button" type="button">
+					Login
+			</button>
+            
+            <input type="text"
+                className="center-rectangle enter"
+                placeholder="Enter your email..." />
+                
+            
+		</div>
+		
+	);
+}
+
+export default Homepage;
