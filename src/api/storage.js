f641c674a99146b5622adf2e0737e55116da89fe
Alan-Qiao <alangxqiao@gmail.com>
2022-03-18 22:35:42 -0400

    Implemented Mock Class Backend
    Mocked using localStorage, structure should be identical to hierarchy for MongoDB.
    - add/remove class
    - add/remove class day for a class
    - add/remove class note for a day in a class
    Other:
    - created redux classList object, not sure how/if to use it

--- src/api/storage.js
+++ src/api/storage.js
@@ -1,8 +1,11 @@
 export const initLocalStorage = () => {
   if (!localStorage.getItem('users')) {
     localStorage.setItem('users', JSON.stringify([]));
+    localStorage.setItem('classes', JSON.stringify([]));
   }
 };
+
+/* User Section */
 
 const findUser = (username, userList) => userList.findIndex(u => u.username === username);
 
@@ -45,4 +48,104 @@ export const deleteUser = username => {
   }
   users.splice(ind, 1);
   localStorage.setItem(users, JSON.stringify(users));
+};
+
+/* Class Section */
+const findById = (id, objList) => objList.findIndex(obj => obj.id === id);
+
+export const createClass = (id, { professor, days }) => {
+  const classes = JSON.parse(localStorage.getItem('classes'));
+  if (findById(id, classes) >= 0) {
+    throw Error('Class already exists!');
+  }
+  classes.push({ id, professor, days: days || [] });
+  localStorage.setItem('classes', JSON.stringify(classes));
+};
+
+export const removeClass = id => {
+  const classes = JSON.parse(localStorage.getItem('classes'));
+  const ind = findById(id, classes);
+  if (ind < 0) {
+    throw Error('Cannot delete nonexistent class!');
+  }
+  classes.splice(ind, 1);
+  localStorage.setItem('classes', JSON.stringify(classes));
+};
+
+/* Class Day Section */
+export const addClassDay = (classId, dayId, { type, date, topic, notes }) => {
+  const classes = JSON.parse(localStorage.getItem('classes'));
+  const classInd = findById(classId, classes);
+  if (classInd < 0) {
+    throw Error('Cannot add day to nonexistent class!');
+  }
+  const { days, ...classAttr } = classes[classInd];
+  days.push({ id: dayId, type, date, topic, notes: notes || [] });
+  classes[classInd] = { ...classAttr, days };
+  localStorage.setItem('classes', JSON.stringify(classes));
+};
+
+export const removeClassDay = (classId, dayId) => {
+  const classes = JSON.parse(localStorage.getItem('classes'));
+  const classInd = findById(classId, classes);
+  if (classInd < 0) {
+    throw Error('Cannot remove day from nonexistent class!');
+  }
+  const { days, ...classAttr } = classes[classInd];
+  const dayInd = findById(dayId, days);
+  if (dayInd < 0) {
+    throw Error('Cannot remove nonexistant day!');
+  }
+  days.splice(dayInd, 1);
+  classes[classInd] = { ...classAttr, days };
+  localStorage.setItem('classes', JSON.stringify(classes));
+};
+
+/* Class Day Notes Section */
+export const addNote = (
+  classId,
+  dayId,
+  noteId,
+  { username, date, body, attachment, likes, comments },
+) => {
+  const classes = JSON.parse(localStorage.getItem('classes'));
+  const classInd = findById(classId, classes);
+  if (classInd < 0) {
+    throw Error('Cannot add note to nonexistent class!');
+  }
+  const { days, ...classAttr } = classes[classInd];
+  const dayInd = findById(dayId, days);
+  if (dayInd < 0) {
+    throw Error('Cannot add note to nonexistant day!');
+  }
+  const { notes, ...dayAttr } = days[dayInd];
+  if (findById(noteId, notes) >= 0) {
+    throw Error('Note already exists!');
+  }
+  notes.push({ noteId, username, date, body, attachment, likes, comments: comments || [] });
+  days[dayInd] = { ...dayAttr, notes };
+  classes[classInd] = { ...classAttr, days };
+  localStorage.setItem('classes', JSON.stringify(classes));
+};
+
+export const removeNote = (classId, dayId, noteId) => {
+  const classes = JSON.parse(localStorage.getItem('classes'));
+  const classInd = findById(classId, classes);
+  if (classInd < 0) {
+    throw Error('Cannot remove note from nonexistent class!');
+  }
+  const { days, ...classAttr } = classes[classInd];
+  const dayInd = findById(dayId, days);
+  if (dayInd < 0) {
+    throw Error('Cannot remove note from nonexistant day!');
+  }
+  const { notes, ...dayAttr } = days[dayInd];
+  const noteInd = findById(noteId, notes);
+  if (noteInd >= 0) {
+    throw Error('Cannot nonexistent note!');
+  }
+  notes.splice(noteInd, 1);
+  days[dayInd] = { ...dayAttr, notes };
+  classes[classInd] = { ...classAttr, days };
+  localStorage.setItem('classes', JSON.stringify(classes));
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import currentUserReducer from './currentUserSlice';
+import classListReducer from './classListSlice';
 
 const store = configureStore({
   reducer: {
     user: currentUserReducer,
+    classList: classListReducer,
   },
 });
 
