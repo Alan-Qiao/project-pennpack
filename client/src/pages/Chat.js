da959949cded26a245ace077f8a0a35a8bf95cbb
Amy Shen <amyshen@seas.upenn.edu>
2022-04-18 16:34:48 -0400

    fixed styling for chatpage, added navbar, updated chatModel, added Contact component

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,24 +1,43 @@
 import { React } from 'react';
 import '../styles/Chat.css';
-// import BackpackEnvelopeNavbar from '../components/BackpackEnvelopeNavbar';
+import Contact from '../components/Contact'
+import BackpackEnvelopeNavbar from '../components/BackpackEnvelopeNavbar';
 
 function Chat() {
+    const username = 'Amy';
+
   return (
 
-    <div className="ChatPage">
-        {/* <BackpackEnvelopeNavbar /> */}
-            <div className="chats-container">
+    // <div className="ChatPage">
+    //     {/* <BackpackEnvelopeNavbar /> */}
+    //         <div className="chats-container">
+    //             <div className="chats-container-title">Chats</div>
+    //             <div className="chats">
+    //                 <Contact username={username}></Contact>
+    //             </div>
+    //         </div>
+    //         <div className="conversation-container">
+    //             <div className="conversation-content">
+    //                 hey
+    //             </div>
+    //         </div>
+    // </div>
+
+    <div className='Chat-Page'>
+        <BackpackEnvelopeNavbar />
+        <div class="main-content">
+            <div class="chats-container">
                 <div className="chats-container-title">Chats</div>
-                <div className="chats">
-                    hey
-                </div>
+                <Contact username={username}></Contact>
             </div>
-            <div className="conversation-container">
-                <div className="conversation-content">
-                    hey
-                </div>
+            
+            <div class="conversation-container">
+                Flex Column 2
             </div>
+  
+        </div>
     </div>
+    
   );
 }
 
