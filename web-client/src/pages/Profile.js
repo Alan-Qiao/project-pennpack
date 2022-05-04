9331ac1874d590f30393bac1c36b312960efd2f7
Amy Shen <amyshen@seas.upenn.edu>
2022-05-04 14:21:19 -0400

    added class grid to profile page

--- web-client/src/pages/Profile.js
+++ web-client/src/pages/Profile.js
@@ -1,34 +1,48 @@
-import { React, useState } from 'react';
+import { React, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/Profile.css';
 import Navbar from '../components/Navbar';
 import ClassGrid from '../components/ClassGrid';
 import { disconnectUser } from '../api/services';
+import { getClassDataById, getUserClasses } from '../components/Class';
 
 function Profile() {
   const navigate = useNavigate();
+  const [userClasses, setUserClasses] = useState([]);
   const user = 'Amy';
   const handle = 'amyshennn';
-  const [course, setCourse] = useState('');
-  const [prof, setProf] = useState('');
-  const [incomplete, setIncomplete] = useState(false);
 
+  const fetchUserClasses = async () => {
+    console.log('in fetchUserClasses in UserDashboard');
+		const allClasses = await getUserClasses();
+    if (allClasses.err) {
+      alert(`An error occured: ${allClasses.err}`)
+    }
+    console.log('allClasses in fetchUserClasses is');
+    console.log(allClasses);
+
+    // Need to get each individual class (need id, classname)
+    for (let i = 0; i < allClasses.length; i++) {
+      const currClass = await getClassDataById(allClasses[i])
+      setUserClasses(oldArray => [...oldArray, currClass]);
+    }
+    
+  }
 
   function logout() {
     disconnectUser();
     navigate('/');
   }
 
-  function handleSubmit() {
-    if (!course || !prof) {
-      setIncomplete(true);
-    }
-  }
-
   const clickedChatWithMe = () => {
     // TODO: ADD NEW CHAT
     navigate('/chat');
   }
+
+  useEffect(() => {
+		fetchUserClasses()
+	}, []);
+
 
   return (
     <>
@@ -48,12 +62,15 @@ function Profile() {
         </div>
         <h5>Analytics</h5>
         <h6>Enrolled classes</h6>
-        {/* ENROLLED CLASSES HERE*/}
+        <ClassGrid classes={userClasses}/>
         <h6>Contributions</h6>
         <div className="notesUploaded">
           Notes uploaded: 4
         </div>
-        <button className="button" type="button" onClick={() => logout()}>
+        <button className="button" 
+                style={{marginBottom: 50}}
+                type="button" 
+                onClick={() => logout()}>
           Logout
         </button>
       </div>
